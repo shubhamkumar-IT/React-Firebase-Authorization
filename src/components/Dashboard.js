a58d7aa63663d04b2290d37776a4060fe66ca870
Shubham kumar <sk15024040@gmail.com>
2022-01-20 23:08:31 +0530

    my changes

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -20,23 +20,27 @@ export default function Dashboard() {
   }
 
   return (
-    <>
-      <Card>
-        <Card.Body>
-          <h2 className="text-center mb-4">Profile</h2>
-          {error && <Alert variant="danger">{error}</Alert>}
-          <strong>Email:</strong> {currentUser.email}
-          <strong>Name:{currentUser.name}</strong>
-          <Link to="/update-profile" className="btn btn-primary w-100 mt-3">
-            Update Profile
-          </Link>
-        </Card.Body>
-      </Card>
-      <div className="w-100 text-center mt-2">
-        <Button variant="link" onClick={handleLogout}>
-          Log Out
-        </Button>
+    <div className="row">
+      <div className="col-md-4">
+        <h2>Users List</h2>
       </div>
-    </>
+      <div className="col-md-8">
+        <Card>
+          <Card.Body>
+            <h2 className="text-center mb-4">Profile</h2>
+            {error && <Alert variant="danger">{error}</Alert>}
+            <strong>Email:</strong> {currentUser.email}
+            <Link to="/update-profile" className="btn btn-primary w-100 mt-3">
+              Update Profile
+            </Link>
+          </Card.Body>
+        </Card>
+        <div className="w-100 text-center mt-2">
+          <Button variant="link" onClick={handleLogout}>
+            Log Out
+          </Button>
+        </div>
+      </div>
+    </div>
   );
 }
